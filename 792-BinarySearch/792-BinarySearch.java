5d4c14330ae6c7d28b63e33745ff100dafbe88d7
Abhay Gupta <157798648+abhay-gupta-199@users.noreply.github.com>
2025-08-06 11:03:14 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,17 @@
+// Last updated: 8/6/2025, 11:03:14 AM
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        while(l <= h) {
+            int mid = (l + h) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(target > nums[mid]) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        } return -1;
+    }
+}
