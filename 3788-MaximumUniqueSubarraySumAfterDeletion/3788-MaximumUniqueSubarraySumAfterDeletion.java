9309faa30d400bdfdf5fcd74cc75efb1fa434cf0
Abhay Gupta <157798648+abhay-gupta-199@users.noreply.github.com>
2025-08-06 10:59:58 +0530

    Create 3788-MaximumUniqueSubarraySumAfterDeletion

---
+++ 3788-MaximumUniqueSubarraySumAfterDeletion/3788-MaximumUniqueSubarraySumAfterDeletion.java
@@ -0,0 +1,18 @@
+// Last updated: 8/6/2025, 10:59:58 AM
+class Solution {
+    public int maxSum(int[] nums) {
+        Arrays.sort(nums);
+        int prev = nums[nums.length - 1];
+        int sum = prev;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int n = nums[i];
+            if (n <= 0) {
+                return sum;
+            } else if (n != prev) {
+                sum = sum + n;
+            }
+            prev = n;
+        }
+        return sum;
+    }
+}
