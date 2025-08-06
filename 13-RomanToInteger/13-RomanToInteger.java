01cd4b48364c2d2e4d04dc50eac9f396679c8656
Abhay Gupta <157798648+abhay-gupta-199@users.noreply.github.com>
2025-08-06 11:05:41 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.java
@@ -0,0 +1,24 @@
+// Last updated: 8/6/2025, 11:05:40 AM
+class Solution {
+    public int romanToInt(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        for(int i = 0; i < n - 1; i++) {
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
+                ans -= map.get(s.charAt(i));
+            } else {
+                ans += map.get(s.charAt(i));
+            }
+        }
+
+        return ans + map.get(s.charAt(n-1));
+    }
+}
