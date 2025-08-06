09a036678dec18f23e17a8543947b7a655e885ed
Abhay Gupta <157798648+abhay-gupta-199@users.noreply.github.com>
2025-08-06 11:00:01 +0530

    Create 3754-MaximumManhattanDistanceAfterKChanges

---
+++ 3754-MaximumManhattanDistanceAfterKChanges/3754-MaximumManhattanDistanceAfterKChanges.java
@@ -0,0 +1,16 @@
+// Last updated: 8/6/2025, 11:00:01 AM
+class Solution {
+    public int maxDistance(String str, int k) {
+        int n = 0, s = 0, e = 0, w = 0, max = 0;
+        for (char c : str.toCharArray()) {
+            n += c == 'N' ? 1 : 0;
+            s += c == 'S' ? 1 : 0;
+            e += c == 'E' ? 1 : 0;
+            w += c == 'W' ? 1 : 0;
+            int y = Math.min(k, Math.min(n, s));
+            int x = Math.min(k - y, Math.min(w, e));
+            max = Math.max(max, Math.abs(n - s) + Math.abs(w - e) + 2*(x + y));
+        }
+        return max;
+    }
+}
