805339f931f52da993f5b36e3e1db7bdefc14472
Abhay Gupta <157798648+abhay-gupta-199@users.noreply.github.com>
2025-08-06 11:01:11 +0530

    Create 2720-MinimizeTheMaximumDifferenceOfPairs

---
+++ 2720-MinimizeTheMaximumDifferenceOfPairs/2720-MinimizeTheMaximumDifferenceOfPairs.java
@@ -0,0 +1,31 @@
+// Last updated: 8/6/2025, 11:01:10 AM
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int left = 0, right = nums[nums.length-1] - nums[0];
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(countPairs(nums, mid) < p){
+                left = mid+1;
+            } else{
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+    private int countPairs(int[] nums, int interval){
+        int idx = 0, count = 0;
+
+        while(idx < nums.length-1){
+            if(nums[idx+1] - nums[idx] <= interval){
+                idx++;
+                count++;
+            }
+            idx++;
+        }
+
+        return count;
+    }
+}
