1ffdbc6144c6d7d2c3d83ae2f292047b110ff3b3
Abhay Gupta <157798648+abhay-gupta-199@users.noreply.github.com>
2025-08-23 17:08:37 +0530

    Time: 3 ms (54.98%) | Memory: 42.4 MB (51.19%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,48 +1,23 @@
 class Solution {
     public String multiply(String a, String b) {
         if(a.equals("0") || b.equals("0")) return "0";
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < a.length(); i++) {
-            int x = a.charAt(i) - '0';
-            int z = 0;
-            StringBuilder pro = new StringBuilder();
-            for(int j = b.length()-1; j >= 0; j--) {
-                int y = b.charAt(j) - '0';
-                int p = x * y + z;
-                int d = p % 10;
-                z = p / 10;
-                pro.insert(0, d);
-            }
-            if(z != 0) pro.insert(0, z);
-            if(res.length() == 0) res.append(pro);
-            else {
-                z = 0;
-                res.append(0);
-                for(int k = 1; k <= pro.length(); k++) {
-                    int c1 = res.charAt(res.length() - k) - '0';
-                    int c2 = pro.charAt(pro.length() - k) - '0';
-                    int p = c1 + c2 + z;
-                    int d = p % 10;
-                    z = p / 10;
-                    res.setCharAt(res.length() - k, (char) (d + '0'));
-                }
-                for(int k = pro.length()+1; k <= res.length(); k++) {
-                    if(z == 0) break;
-                    int c1 = res.charAt(res.length() - k) - '0';
-                    int p = c1 + z;
-                    int d = p % 10;
-                    z = p / 10;
-                    res.setCharAt(res.length() - k, (char) (d + '0'));
-                }
-                if(z != 0) res.insert(0, z);
+        int n = a.length();
+        int m = b.length();
+        int[] prod = new int[n + m];
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j >= 0; j--) {
+                int p = (a.charAt(i) - '0') * (b.charAt(j) - '0');
+                int s = prod[i+j+1] + p;
+                prod[i+j] += s / 10;
+                prod[i+j+1] = s % 10;
             }
         }
-        for(int i = 0; i < res.length(); i++) {
-            int ch = res.charAt(i) - '0';
-            if(ch != 0) {
-                res.delete(0, i);
-                break;
-            }
+
+        StringBuilder res = new StringBuilder();
+        for(int x : prod) {
+            if(res.length() == 0 && x == 0) continue;
+            res.append(x);
         }
         return res.toString();
     }
