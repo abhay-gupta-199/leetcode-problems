adbbf6b3a57235e9b06081c14cf58d20fac3ff59
Abhay Gupta <157798648+abhay-gupta-199@users.noreply.github.com>
2025-08-28 12:15:52 +0530

    Create 502-Ipo

---
+++ 502-Ipo/502-Ipo.java
@@ -0,0 +1,24 @@
+// Last updated: 8/28/2025, 12:15:51 PM
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = capital.length;
+        int[][] arr = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            arr[i][0] = capital[i];
+            arr[i][1] = profits[i];
+        }
+    
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+
+        for(int i = 0; i <= n; ) {
+            while(i < n && w >= arr[i][0]) {
+                pq.add(arr[i++]);
+            }
+            if(pq.isEmpty() || k == 0) return w;
+            w += pq.poll()[1];
+            k--;
+        }
+        return w;
+    }
+}
