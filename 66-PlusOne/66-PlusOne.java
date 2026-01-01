5ab8d94718d1ffe71a6f6387898f1156bf08c9a8
Abhay Gupta <157798648+abhay-gupta-199@users.noreply.github.com>
2026-01-01 11:01:55 +0530

    File updated at 1/1/2026, 11:01:55 AM

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,15 +1,15 @@
-// Last updated: 8/6/2025, 11:04:54 AM
-class Solution {
-    public int[] plusOne(int[] digits) {
-        int[] dig = new int[digits.length + 1];
-        for(int i = digits.length - 1; i >= 0; i--) {
-            digits[i] = (digits[i] + 1) % 10;
-            dig[i+1] = digits[i];
-            if(digits[i] != 0) {
-                return digits;
-            }
-        }
-        dig[0] = 1;
-        return dig;
-    }
-}
+// Last updated: 1/1/2026, 11:01:55 AM
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        int[] dig = new int[digits.length + 1];
+4        for(int i = digits.length - 1; i >= 0; i--) {
+5            digits[i] = (digits[i] + 1) % 10;
+6            dig[i+1] = digits[i];
+7            if(digits[i] != 0) {
+8                return digits;
+9            }
+10        }
+11        dig[0] = 1;
+12        return dig;
+13    }
+14}
